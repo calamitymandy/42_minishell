191549cfe43a2cd8b8ba8a285720f28d21ad33ad
Amandine Demuynck <amdemuyn@c1r13s4.42madrid.com>
2024-06-12 20:08:26 +0200

    starting structs

--- main.c
+++ main.c
@@ -6,23 +6,37 @@
 /*   By: amdemuyn <amdemuyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 20:24:50 by amdemuyn          #+#    #+#             */
-/*   Updated: 2024/06/06 21:10:43 by amdemuyn         ###   ########.fr       */
+/*   Updated: 2024/06/12 19:51:21 by amdemuyn         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
+#include "minishell.h"
 /*compile with gcc main.c -lreadline*/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+int g_status;
 
-#define MAX_INPUT_SIZE 1024
+void    exit_mini(t_minishell *mini, int exit_code)
+{
+    //TODO
+    exit(exit_code);
+}
 
-int main() {
-    char *input;
+bool    init_main_struct(t_minishell *mini, char **env)
+{
+    //TODO set_env_var & set_pwd_and aldpwd
+    mini->env = env;
+    mini->line = NULL;
+    //mini->ctrlc_heredoc = false;
+    //mini->token = NULL;
+    //mini->cmd = NULL;
+    mini->pid = -1;
+    g_status = 0;
+    return (true);
+}
+
+void    main_loop(t_minishell *mini)
+{
+    char    *input;
 
     while (1) {
         input = readline("$-> ");
@@ -48,6 +62,16 @@ int main() {
         }
         free(input);
     }
+}
 
+int main(int argc, char **argv, char **env)
+{
+    t_minishell mini;
+
+    (void)argv;
+    (void)argc;
+    if (!init_main_struct(&mini, env))
+        exit_mini(NULL, EXIT_FAILURE);
+    main_loop(&mini);
     return 0;
 }
