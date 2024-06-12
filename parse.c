0310b6d725b7a17332e21853f7805fc8ef216fbc
Alvaro Galiana Albores <algalian@c1r13s6.42madrid.com>
2024-06-12 18:01:15 +0200

    makefile

--- parse.c
+++ parse.c
@@ -6,11 +6,18 @@
 int main(int argc, char **argv)
 {
 	int i;
+	int j;
+	char *input;
 	//try to handle the $
 	
-	while(argv[i])
+	while(argv[i][j])
     {
+		if(argv[i][j])
+		{
 
+			j++;
+		}
+		i++;
 	}
 	return(0);
 }
