d0dbdca2c0558f9e1254345663f9217223bea880
Alvaro Galiana Albores <algalian@c1r14s5.42madrid.com>
2024-06-25 14:42:11 +0200

    expand

---
+++ expand.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"./libft/libft.h"
+
+
+char  *expand(char *s)
+{
+	int	i;
+	int	j;
+	char *var = "VARIABLE";
+	char *alias = "asdf";
+	int	len;
+	char *x;
