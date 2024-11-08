4a7ad0e17e0fecb53a6959eda114e1a483bde117
algalian <algalian@DESKTOP-7RPJL64>
2024-11-08 18:26:00 +0100

    compiling

--- close_n_free.c
+++ close_n_free.c
@@ -42,3 +42,56 @@ void	free_two_stars(char **arr)
 		arr = NULL;
 	}
 }
+
+bool	ms_reset_io(t_fds *io)
+{
+	int	res;
+
+	res = true;
+	if (!io)
+		return (res);
+	if (io->stdin_backup != -1)
+	{
+		if (dup2(io->stdin_backup, STDIN_FILENO) == -1)
+			res = false;
+		close(io->stdin_backup);
+		io->stdin_backup = -1;
+	}
+	if (io->stdout_backup != -1)
+	{
+		if (dup2(io->stdout_backup, STDOUT_FILENO) == -1)
+			res = false;
+		close(io->stdout_backup);
+		io->stdout_backup = -1;
+	}
+	return (res);
+}
+
+void	ms_close_un_pipes_fd(t_command *cmds, t_command *omit_cmd)
+{
+	while (cmds)
+	{
+		if (cmds != omit_cmd && cmds->pipe_fd)
+		{
+			close(cmds->pipe_fd[0]);
+			close(cmds->pipe_fd[1]);
+		}
+		cmds = cmds->next;
+	}
+}
+
+
+
+void	ms_close_fds(t_command *cmds, bool close_backups)
+{
+	if (cmds->fds)
+	{
+		if (cmds->fds->fd_in != -1)
+			close(cmds->fds->fd_in);
+		if (cmds->fds->fd_out != -1)
+			close(cmds->fds->fd_out);
+		if (close_backups)
+			ms_reset_io(cmds->fds);
+	}
+	ms_close_un_pipes_fd(cmds, NULL);
+}
