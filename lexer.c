24060a2baae2c402573ecdd70ca3efd0cc536b67
algalian <algalian@DESKTOP-7RPJL64>
2024-11-28 13:38:53 +0100

    lexer & parser minor changes

--- lexer.c
+++ lexer.c
@@ -122,13 +122,6 @@ bool	stx_err(t_token **token_list)
 	return (false);
 }
 
-
-
-
-
-
-
-
 char	*replace_str_heredoc(char *str, char *var_value, int index)
 {
 	char	*tmp;
@@ -146,9 +139,6 @@ char	*replace_str_heredoc(char *str, char *var_value, int index)
 	return (str);
 }
 
-
-
-
 void	token_indx(t_minishell *ms)
 {
 	t_token			*aux;
@@ -158,7 +148,7 @@ void	token_indx(t_minishell *ms)
 	i = 0;
 	while (aux->next)
 	{
-		aux->index += i; // why "+="? where are the previous indexes coming from?
+		aux->index += i;
 		i++;
 		aux = aux->next;
 	}
--- parser.c
+++ parser.c
@@ -681,7 +681,7 @@ void	pipe_parser(t_minishell *ms, t_token **token_lst)
 {
 	t_command	*last_cmd;
 
-	last_cmd = scroll_lstcmd(ms->command);
+last_cmd = scroll_lstcmd(ms->command);
 	last_cmd->pipe_output = true;
 	addlst_cmd_container(ms, &last_cmd);
 	*token_lst = (*token_lst)->next;
