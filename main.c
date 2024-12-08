28c5e84a26dcafdca7f350af63bf4265f8077d3a
calamitymandy <amandined.plateforme@gmail.com>
2024-12-08 23:23:14 +0100

    coms on export

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: amdemuyn <amdemuyn@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 20:24:50 by amdemuyn          #+#    #+#             */
-/*   Updated: 2024/12/07 20:39:11 by amdemuyn         ###   ########.fr       */
+/*   Updated: 2024/12/08 20:15:33 by amdemuyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -609,6 +609,28 @@ char	*add_env_quotes(char *env_var)
 	return (quoted_env_var);
 }
 
+/* 
+1-Check for Null Environment: If mini->env is NULL return EXIT_FAILURE.
+2-Count Environment Variables: Total number of environment variables 
+in mini->env is found using nb_env_variables(mini->env).
+3-Sort Environment Variables: The array of environment variables is 
+sorted alphabetically using qsort_env_vars(mini->env, nb_env_var).
+This ensures a consistent and organized display of variables.
+4-Display Environment Variables: while loop iterates over each 
+environment variable.
+For each variable:
+add_env_quotes() is called to add necessary quotes around the 
+variable's value for proper formatting. The variable is then printed 
+to the console with the format declare -x <variable>.
+ex:
+"PATH=/usr/bin" becomes "declare -x PATH=\"/usr/bin\""
+"USER=john" becomes "declare -x USER=\"john\""
+Prints:
+declare -x PATH="/usr/bin"
+declare -x USER="john"
+5-Return Success: After successfully processing and displaying all 
+variables, the function returns EXIT_SUCCESS to indicate success.*/
+
 int	export_builtin(t_minishell *mini)
 {
 	char 	*env_quotes;
@@ -630,6 +652,36 @@ int	export_builtin(t_minishell *mini)
 	return (EXIT_SUCCESS);
 }
 
+/* For ex, if the command is export VAR=value, 
+then args would be something like ["export", "VAR=value", NULL]
+1- Initialization: res is set to EXIT_SUCCESS initially & 
+i starts at 1 to skip the command name (args[0]).
+
+2-Check for No Arguments:
+If args[1] is NULL, it means no arguments were passed si it calls 
+export_builtin(mini) to display all exported variables) and returns
+its result.
+
+3-Iterating Over Arguments: The while loop processes each argument.
+
+4-Validation of Environment Key:
+checks if the current argument (args[i]) is a valid environment 
+variable name using valid_env_key().
+If invalid: error_msg() & res is updated to EXIT_FAILURE.
+
+5-Handling Key-Value Pairs:
+If the argument contains = (detected using ft_strchr()): split it
+into a key-value pair using key_value_arr(). The key-value pair 
+is added or updated in the environment using add_or_update_env_var().
+The temporary array (arr) is freed using free_two_stars().
+
+6-Loop Increment:
+i is incremented to process the next argument.
+
+7-Return Value:
+After processing all arguments, it returns res, which indicates 
+the overall success (EXIT_SUCCESS) or failure (EXIT_FAILURE)
+*/
 
 int	exec_export_builtin(t_minishell *mini, char **args)
 {
