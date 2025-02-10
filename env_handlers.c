2eb5c8d9fe7615223ae9bb4f1d16d2a236db781c
calamitymandy <amandined.plateforme@gmail.com>
2025-02-10 18:41:18 +0100

    cleaning and putting order

--- env_handlers.c
+++ env_handlers.c
@@ -3,14 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   env_handlers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amdemuyn <amdemuyn@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: amdemuyn <amdemuyn@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 17:31:14 by amdemuyn          #+#    #+#             */
-/*   Updated: 2025/01/28 17:32:04 by amdemuyn         ###   ########.fr       */
+/*   Updated: 2025/02/10 18:13:36 by amdemuyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+bool	copy_environment(t_minishell *ms, char **envp)
+{
+	int		i;
+
+	if (*envp)
+		ms->env = ft_arrdup(envp);
+	modify_shlvl(ms);
+	refresh_pwd_env(ms);
+	if (!ms->env)
+		return (false);
+	i = 0;
+	while (ms->env[i])
+	{
+		if (!ms->env[i])
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+void	refresh_pwd_env(t_minishell *mini)
+{
+	char	*pwd;
+	char	*str;
+
+	pwd = search_env(mini, "PWD");
+	if (!pwd)
+	{
+		str = getcwd(NULL, 0);
+		mini->env = arr_append(mini->env, str);
+		free(str);
+		return ;
+	}
+}
 
 char	**callocate_env_variables(t_minishell *mini, int size)
 {
